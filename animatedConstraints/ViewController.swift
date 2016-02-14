dc20cfbd7785ca79f2d77cd477765a4330a54c2c
Akilesh Bapu <akilesh@berkeley.edu>
2016-02-13 23:06:45 -0800

    Initial Commit

---
+++ animatedConstraints/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  animatedConstraints
+//
+//  Created by Akilesh Bapu on 2/13/16.
+//  Copyright © 2016 justforfun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
