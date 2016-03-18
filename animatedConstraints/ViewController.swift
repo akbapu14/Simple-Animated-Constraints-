63dd781eadd7b4b1df35ff123bf67df832f2b396
Akilesh Bapu <akilesh@berkeley.edu>
2016-03-17 23:04:25 -0700

    INIT

--- animatedConstraints/ViewController.swift
+++ animatedConstraints/ViewController.swift
@@ -10,9 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftSpace: NSLayoutConstraint!
+    @IBOutlet weak var downToButton: NSLayoutConstraint!
+    @IBOutlet weak var boxToTop: NSLayoutConstraint!
+    @IBOutlet weak var alignLeft: NSLayoutConstraint!
+    @IBAction func moveConstraints(sender: AnyObject) {
+        
+        boxToTop.constant = boxToTop.constant - 50
+        downToButton.constant += 50
+        leftSpace.constant += 300
+        print(view.widthAnchor)
+        if (leftSpace.constant >= view.frame.width) {
+            leftSpace.constant -= 400
+        }
+        if (boxToTop.constant <= 0) {
+            boxToTop.constant += 200
+            downToButton.constant -= 200
+        }
+        UIView.animateWithDuration(0.5) {
+            self.view.layoutIfNeeded()        }
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func viewDidAppear(animated: Bool) {
+        var save: CGFloat = boxToTop.constant
+        boxToTop.constant = 0
+        while (boxToTop.constant <= save) {
+            boxToTop.constant += 40
+            UIView.animateWithDuration(0.5) {
+                self.view.layoutIfNeeded()
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
